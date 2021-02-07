1a411ef1dcb7b84a1e830b627cdd6d9eb71dc81d
Ricardo Nascimento <ricardobatistanf@gmail.com>
2021-02-07 00:55:44 -0300

    First Commit

---
+++ src/Person.kt
@@ -0,0 +1,9 @@
+data class Person(
+    val id: Long,
+    val firstName: String,
+    val lastName: String,
+    val age: Int?
+) // Data classes não possuem ou não faz operaçaões
+
+// É gerado automaticamente os GETTERS e SETTERS do JAVA
+
